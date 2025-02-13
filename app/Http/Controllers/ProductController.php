be7bfa4e8bcad6d4b80d53dc24444cee1c79587c
Madhvik <madhvikkathiria@gmail.com>
2025-02-13 11:34:26 +0530

    First Commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,77 @@
+<?php
+ 
+namespace App\Http\Controllers;
+ 
+use Illuminate\Http\Request;
+use App\Models\Product;
+ 
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::orderBy('id', 'desc')->get();
+        $total = Product::count();
+        return view('admin.product.home', compact(['products', 'total']));
+    }
+ 
+    public function create()
+    {
+        return view('admin.product.create');
+    }
+ 
+    public function save(Request $request)
+    {
+        $validation = $request->validate([
+            'title' => 'required|string',
+            'category' => 'required|string',
+            'price' => 'required|integer',
+        ]);
+        $data = Product::create($validation);
+        if ($data) {
+            session()->flash('success', 'Product Add Successfully');
+            return redirect(route('admin/products'));
+        } else {
+            session()->flash('error', 'Some problem occure');
+            return redirect(route('admin.products/create'));
+        }
+    }
+    public function edit($id)
+    {
+        $products = Product::findOrFail($id);
+        return view('admin.product.update', compact('products'));
+    }
+ 
+    public function delete($id)
+{
+    $product = Product::findOrFail($id);
+    
+    if ($product->delete()) {
+        session()->flash('success', 'Product Deleted Successfully');
+    } else {
+        session()->flash('error', 'Product Not Deleted Successfully');
+    }
+    
+    return redirect()->route('admin/products');
+}
+
+ 
+    public function update(Request $request, $id)
+    {
+        $products = Product::findOrFail($id);
+        $title = $request->title;
+        $category = $request->category;
+        $price = $request->price;
+ 
+        $products->title = $title;
+        $products->category = $category;
+        $products->price = $price;
+        $data = $products->save();
+        if ($data) {
+            session()->flash('success', 'Product Update Successfully');
+            return redirect(route('admin/products'));
+        } else {
+            session()->flash('error', 'Some problem occure');
+            return redirect(route('admin/products/update'));
+        }
+    }
+}
